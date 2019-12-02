7a94810d2c65750573dac43dd26ab7be7f656501
MarianPjajcik <pjajcik@cngroup.dk>
2019-12-02 16:34:44 +0100

    daily backup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from "react";
+import { useState } from "react";
 import AppTemplate from "./components/templates/AppTemplate";
 
-
-
 function App() {
+  const [minutes, setMinutes] = useState(1);
+  const [seconds, setSeconds] = useState(1);
+
+  const timeCounter = (minutes, seconds) => {
+
+  }
 
   return (
-    <AppTemplate/>
+    <AppTemplate
+      onMinutesChange={setMinutes}
+      onSecondsChange={setSeconds}
+      minutes={minutes}
+      seconds={seconds}
+    />
   );
 }
 
--- src/components/templates/AppTemplate.js
+++ src/components/templates/AppTemplate.js
@@ -1,27 +1,72 @@
 import React from "react";
 import styled from "@emotion/styled";
 import "antd/dist/antd.css";
-import { Typography, Layout } from "antd";
-import { Row, Col } from "antd";
+import { Typography, Layout, Row, Col, InputNumber, Button } from "antd";
 
-//const { Title } = Typography;
-
-const Title = styled(Typography)({
-  color: "#FFFCF2"
-});
+const { Title } = Typography;
 
 const { Header, Footer, Content } = Layout;
 
-const AppTemplate = () => {
+const AppTemplate = props => {
+  const { onMinutesChange, onSecondsChange, minutes, seconds } = props;
   return (
     <Layout>
       <Header>
         <Title>Pomodorify</Title>
       </Header>
       <Content>
-        <Row>
-          <Col span={8}>ahoj</Col>
-          <Col span={8}>kamarade</Col>
+        <Row type="flex" align="middle">
+          <Col offset={1} span={3}>Focus time:</Col>
+          <Col span={2}>
+            <InputNumber
+              min={1}
+              max={60}
+              defaultValue={1}
+              value={minutes}
+              onChange={event => onMinutesChange(event)}
+            />
+          </Col>
+          <Col span={3}>Minutes</Col>
+          <Col span={2}>
+            <InputNumber
+              min={1}
+              max={59}
+              defaultValue={1}
+              value={seconds}
+              onChange={event => onSecondsChange(event)}
+            />
+          </Col>
+          <Col span={3}>Seconds</Col>
+        </Row>
+
+        <Row type="flex">
+          <Col offset={11}>
+            <Button type="primary">GO!</Button>
+          </Col>
+        </Row>
+
+        <Row type="flex" align="middle">
+          <Col offset={2} span={2}>
+            <Title level={3}>Minutes</Title>
+          </Col>
+          <Col span={1}>
+            <Title level={3}>{minutes}</Title>
+          </Col>
+          <Col span={1}>
+            <Title level={3}>:</Title>
+          </Col>
+          <Col span={1}>
+            <Title level={3}>{seconds}</Title>
+          </Col>
+          <Col span={1}>
+            <Title level={3}>Seconds</Title>
+          </Col>
+        </Row>
+
+        <Row type="flex">
+          <Col offset={11}>
+            <Button type="danger">STOP!</Button>
+          </Col>
         </Row>
       </Content>
       <Footer></Footer>
