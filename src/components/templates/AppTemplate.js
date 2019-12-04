714de8c0770d02096336691d724eda09b0ea0bbf
MPjajcik <pjajczik@gmail.com>
2019-12-04 20:11:39 +0100

    build

--- src/components/templates/AppTemplate.js
+++ src/components/templates/AppTemplate.js
@@ -5,14 +5,12 @@ import { Typography, Layout, InputNumber, Button } from "antd";
 
 const { Title } = Typography;
 
-
 const { Header, Footer, Content } = Layout;
 
 const SiteWrapper = styled.div({
   display: "flex",
   margin: "0 auto",
-  flexDirection: "column",
-  alignItems: "center"
+  flexDirection: "column"
 });
 
 const RowWrapper = styled.div({
@@ -23,13 +21,16 @@ const RowWrapper = styled.div({
 const ButtonsWrapper = styled.div({
   display: "flex",
   marginTop: "15px",
-  justifyContent: "space-around"
+  justifyContent: "space-between",
+  alignItems: "center"
 });
 
 const FooterWrapper = styled.div({
   display: "flex",
   justifyContent: "flex-end"
-})
+});
+
+
 
 const AppTemplate = props => {
   const {
@@ -41,7 +42,7 @@ const AppTemplate = props => {
   } = props;
   return (
     <SiteWrapper>
-      <Layout style={{height:"70vh"}}>
+      <Layout style={{ height: "85vh" }}>
         <Header>
           <RowWrapper>
             <Title style={{ color: "white" }}>
@@ -54,32 +55,36 @@ const AppTemplate = props => {
           </RowWrapper>
         </Header>
 
-        <Content >
+        <Content>
           <RowWrapper>
-            <Title level={3} style={{ margin: "0 5px" }}>
-              Minutes
-            </Title>
-            <InputNumber
-              min={0}
-              max={59}
-              defaultValue={0}
-              value={minutes}
-              onChange={event => onMinutesChange(event)}
-            />
-            <Title level={3} style={{ margin: "0 10px" }}>
-              :
-            </Title>
-            <InputNumber
-              min={0}
-              max={59}
-              defaultValue={0}
-              value={seconds}
-              onChange={event => onSecondsChange(event)}
-            />
-            <Title level={3} style={{ margin: "0 5px" }}>
-              Seconds
-            </Title>
+            <RowWrapper>
+              <Title level={3} style={{ margin: "0 5px" }}>
+                Minutes
+              </Title>
+              <InputNumber
+                min={0}
+                max={59}
+                defaultValue={0}
+                value={minutes}
+                onChange={event => onMinutesChange(event)}
+              />
+            </RowWrapper>
           </RowWrapper>
+          <RowWrapper>
+            <RowWrapper>
+              <Title level={3} style={{ margin: "0 5px" }}>
+                Seconds
+              </Title>
+              <InputNumber
+                min={0}
+                max={59}
+                defaultValue={0}
+                value={seconds}
+                onChange={event => onSecondsChange(event)}
+              />
+            </RowWrapper>
+          </RowWrapper>
+
           <ButtonsWrapper>
             <Button type="primary" onClick={onGoClick}>
               GO!
@@ -90,10 +95,10 @@ const AppTemplate = props => {
 
         <Footer>
           <FooterWrapper>
-          Pomodorify, made with love{" "}
-          <span role="img" aria-label="heart">
-            ❤️
-          </span>
+            Pomodorify, made with love{" "}
+            <span role="img" aria-label="heart">
+              ❤️
+            </span>
           </FooterWrapper>
         </Footer>
       </Layout>
